74669859d3083cdb0da5f56a402809a6860eaf5d
Wesley <fariawesley4@gmail.com>
2021-11-24 22:31:21 -0300

    Exercise 2.67

--- 2.67/main.c
+++ 2.67/main.c
@@ -28,3 +28,32 @@
 // B. Modify the code to run properly on any machine for which data type int is at least 32 bits.
 
 // C. Modify the code to run properly on any machine for which data type int is at least 16 bits.
+
+// Answers based on:
+// https://valineliu.gitbook.io/deuterium-wiki/reading/cs-jing-dian-shu-ji/csapp-3e-homework-solution/2.-representing-and-manipulating-information/2.67-int-size-is-32
+
+// A - answear.
+// If the value of the right operand is negative or is greater than or equal to the width of the
+// promoted left operand, the behavior it undefined
+
+#include <stdio.h>
+
+int int_size_is_32()
+{
+	int set_msb = 1 << 31;
+	int beyond_msb = set_msb << 1;
+	return set_msb && !beyond_msb;
+}
+
+int int_size_is_32_16bits()
+{
+	int set_msb = 1 << 15 << 15 << 1;
+	int beyond_msb = set_msb << 1;
+	return set_msb && !beyond_msb;
+}
+
+int main()
+{
+	int Result = int_size_is_32();
+	printf("%d", Result);
+}
---
+++ 2.68/main.c
@@ -0,0 +1,15 @@
+// Dificulty: **
+// Expected 20 min
+
+// Write code for a function with the following prototype:
+
+//
+//	Mask with least significant n bits set to 1
+//	Examples: n = 6 ---> 0x2F, n = 17 --> 0x1FFFF
+//	Assume 1 <= n <= w
+//
+
+// int lower_one_mask(int n);
+
+// Your function should follow the bit level integer coding rules (page 120). Be careful of the
+// case n = w.
