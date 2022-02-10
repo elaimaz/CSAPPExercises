ac4a6770433afef3aeee40fa875badcc0c5c0327
wesley_cazelli <wesley_cazelli@hst.com.br>
2022-02-10 17:47:28 -0300

    :pencil: 2.79 done

--- 2.79/main.c
+++ 2.79/main.c
@@ -1,9 +1,31 @@
+// Based on:
+// https://github.com/agelessman/csapp-3e-solutions/blob/master/%E7%AC%AC%E4%BA%8C%E7%AB%A0%E7%AD%94%E6%A1%88/2.80.c
+
+#include <limits.h>
+#include <assert.h>
+
 int threefourths(int x)
 {
+	int is_neg = x & INT_MIN;
 
+	int f = x & ~0x3;
+	int l = x & 0x3;
+
+	int fd4 = f >> 2;
+	int fd4m3 = (fd4 << 1) + fd4;
+
+	int lm3 = (l << 1) + l;
+	int bias = (1 << 1) + 1;
+	(is_neg && (lm3 += bias));
+	int lm3d4 = lm3 >> 2;
+
+	return fd4m3 + lm3d4;
 }
 
 int main()
 {
+	assert(threefourths(8) == 6);
+	assert(threefourths(-8) == -6);
+
 	return 0;
 }
---
+++ 2.80/redme.md
@@ -0,0 +1,20 @@
+Exercise 2.80
+==============
+
+### ***Dificulty***: :star: :star:
+
+---
+
+### ***Expected time***: ***20 min*** :hourglass_flowing_sand:
+
+---
+
+### ***Question***:
+
+Write C expressions to generate the bit patterns that follow, where $a^{k}$ represents k
+repetitions of symbol a. Assume a w-bit data type. Your code may contain references to parameters
+j and k representing the values of j and k, but not a parameter representing w.
+
+A. $1^{w-k}0^{k}$
+
+B. $0^{w-k-j}1^{k}0^{j}$
