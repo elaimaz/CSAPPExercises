48c289ef13de4e4eedcaf229f8adf14d098b47b2
elaimaz <fariawesley4@gmail.com>
2023-08-27 16:09:20 -0300

    :white_check_mark: 3.54 done

--- Chapter 3/3.54/main.c
+++ Chapter 3/3.54/main.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <assert.h>
+
 int decode2(int x, int y, int z) {
     y -= z;
     
@@ -11,6 +14,28 @@ int decode2(int x, int y, int z) {
     return y ^ z;
 }
 
+int decode2_asm(int x, int y, int z) {
+    int eaxValue = 0;
+
+    //Intel syntax
+    __asm {
+        mov edx, y
+        sub edx, z
+        mov eax, edx
+        sal eax, 31
+        sar eax, 31
+        imul edx, x
+        xor eax, edx
+        mov eaxValue, eax
+    }
+
+    return eaxValue;
+}
+
 int main() {
-  return 0;
+    assert(decode2(1, 2, 3) == decode2_asm(1, 2, 3));
+    assert(decode2(3, 2, 1) == decode2_asm(3, 2, 1));
+    assert(decode2(-537, 885, 199) == decode2_asm(-537, 885, 199));
+
+    return 0;
 }
