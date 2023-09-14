8c67e36316f45c07dc7c716c85bf93fa9626fa02
wesley_cazelli <wesley_cazelli@hst.com.br>
2023-09-14 16:04:25 -0300

    :white_check_mark: 3.59 done

--- Chapter 3/3.59/main.c
+++ Chapter 3/3.59/main.c
@@ -4,8 +4,22 @@ int switch_prob(int x, int n)
 
 	switch (n)
 	{
-	/* Fill in code here */
+	case 32:
+	case 34:
+		result <<= 2;
+		break;
+	case 35:
+		result >>= 2;
+		break;
+	case 36:
+		result += (result * 2);
+	case 37:
+		result *= result;
+	default:
+		result += 10;
 	}
+
+	return result;
 }
 
 int main() {
