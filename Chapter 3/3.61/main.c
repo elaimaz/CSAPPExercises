d177d7573b67eb5067635d200f8d343c300d7056
elaimaz <fariawesley4@gmail.com>
2023-09-20 13:37:46 -0300

    :construction: 3.61 adjustments

--- Chapter 3/3.61/main.c
+++ Chapter 3/3.61/main.c
@@ -1,11 +1,10 @@
 int var_prod_ele(int n, int A[n][n], int B[n][n], int i, int k) {
     int j;
-    int result = 0;
 
     for (j = 0; j < n; j++)
         A[i][0] += A[i][j] * B[j][k];
 
-    return A[i][0];
+    return j < n ? A[i][0] : 0;
 }
 
 int main() {
--- Chapter 3/3.61/main.s
+++ Chapter 3/3.61/main.s
@@ -1,5 +1,3 @@
-#Compiled with gcc -c -m32 -S -fverbose-asm main.c
-
 	.file	"main.c"
 # GNU C17 (Ubuntu 11.4.0-1ubuntu1~22.04) version 11.4.0 (x86_64-linux-gnu)
 #	compiled by GNU C version 11.4.0, GMP version 6.2.1, MPFR version 4.1.0, MPC version 1.2.1, isl version isl-0.24-GMP
@@ -27,81 +25,88 @@ var_prod_ele:
 	call	__x86.get_pc_thunk.ax	#
 	addl	$_GLOBAL_OFFSET_TABLE_, %eax	# tmp82,
 # main.c:1: int var_prod_ele(int n, int A[n][n], int B[n][n], int i, int k) {
-	movl	8(%ebp), %eax	# n, n.0_47
+	movl	8(%ebp), %eax	# n, n.0_48
 	leal	-1(%eax), %edx	#, _1
 # main.c:1: int var_prod_ele(int n, int A[n][n], int B[n][n], int i, int k) {
-	movl	%edx, -28(%ebp)	# _2, D.1550
+	movl	%edx, -24(%ebp)	# _2, D.1549
 # main.c:1: int var_prod_ele(int n, int A[n][n], int B[n][n], int i, int k) {
-	movl	8(%ebp), %edx	# n, n.3_51
+	movl	8(%ebp), %edx	# n, n.3_52
 	leal	-1(%edx), %ecx	#, _8
 # main.c:1: int var_prod_ele(int n, int A[n][n], int B[n][n], int i, int k) {
-	movl	%ecx, -24(%ebp)	# _9, D.1554
-# main.c:3:     int result = 0;
-	movl	$0, -20(%ebp)	#, result
-# main.c:5:     for (j = 0; j < n; j++)
-	movl	$0, -32(%ebp)	#, j
-# main.c:5:     for (j = 0; j < n; j++)
+	movl	%ecx, -20(%ebp)	# _9, D.1553
+# main.c:4:     for (j = 0; j < n; j++)
+	movl	$0, -28(%ebp)	#, j
+# main.c:4:     for (j = 0; j < n; j++)
 	jmp	.L2	#
 .L3:
-# main.c:6:         A[i][0] += A[i][j] * B[j][k];
-	movl	%eax, %ebx	# n.0_47, n.8_15
+# main.c:5:         A[i][0] += A[i][j] * B[j][k];
+	movl	%eax, %ebx	# n.0_48, n.8_15
 	movl	20(%ebp), %ecx	# i, i.9_16
 	imull	%ebx, %ecx	# n.8_15, _17
 	leal	0(,%ecx,4), %ebx	#, _18
 	movl	12(%ebp), %ecx	# A, tmp133
 	addl	%ecx, %ebx	# tmp133, _19
-# main.c:6:         A[i][0] += A[i][j] * B[j][k];
-	movl	-32(%ebp), %ecx	# j, tmp134
+# main.c:5:         A[i][0] += A[i][j] * B[j][k];
+	movl	-28(%ebp), %ecx	# j, tmp134
 	movl	(%ebx,%ecx,4), %ebx	# (*_19)[j_44], _20
-# main.c:6:         A[i][0] += A[i][j] * B[j][k];
-	movl	%edx, %esi	# n.3_51, n.10_21
-	movl	-32(%ebp), %ecx	# j, j.11_22
+# main.c:5:         A[i][0] += A[i][j] * B[j][k];
+	movl	%edx, %esi	# n.3_52, n.10_21
+	movl	-28(%ebp), %ecx	# j, j.11_22
 	imull	%esi, %ecx	# n.10_21, _23
 	leal	0(,%ecx,4), %esi	#, _24
 	movl	16(%ebp), %ecx	# B, tmp135
 	addl	%ecx, %esi	# tmp135, _25
-# main.c:6:         A[i][0] += A[i][j] * B[j][k];
+# main.c:5:         A[i][0] += A[i][j] * B[j][k];
 	movl	24(%ebp), %ecx	# k, tmp136
-	movl	(%esi,%ecx,4), %ecx	# (*_25)[k_64(D)], _26
-# main.c:6:         A[i][0] += A[i][j] * B[j][k];
+	movl	(%esi,%ecx,4), %ecx	# (*_25)[k_66(D)], _26
+# main.c:5:         A[i][0] += A[i][j] * B[j][k];
 	movl	%ebx, %esi	# _20, _20
 	imull	%ecx, %esi	# _26, _20
-# main.c:6:         A[i][0] += A[i][j] * B[j][k];
-	movl	%eax, %ebx	# n.0_47, n.12_27
-# main.c:6:         A[i][0] += A[i][j] * B[j][k];
+# main.c:5:         A[i][0] += A[i][j] * B[j][k];
+	movl	%eax, %ebx	# n.0_48, n.12_27
+# main.c:5:         A[i][0] += A[i][j] * B[j][k];
 	movl	20(%ebp), %ecx	# i, i.13_28
 	imull	%ebx, %ecx	# n.12_27, _29
 	leal	0(,%ecx,4), %ebx	#, _30
 	movl	12(%ebp), %ecx	# A, tmp137
 	addl	%ebx, %ecx	# _30, _31
 	movl	(%ecx), %ebx	# (*_31)[0], _32
-# main.c:6:         A[i][0] += A[i][j] * B[j][k];
-	movl	%eax, %edi	# n.0_47, n.14_33
-# main.c:6:         A[i][0] += A[i][j] * B[j][k];
+# main.c:5:         A[i][0] += A[i][j] * B[j][k];
+	movl	%eax, %edi	# n.0_48, n.14_33
+# main.c:5:         A[i][0] += A[i][j] * B[j][k];
 	movl	20(%ebp), %ecx	# i, i.15_34
 	imull	%edi, %ecx	# n.14_33, _35
 	leal	0(,%ecx,4), %edi	#, _36
 	movl	12(%ebp), %ecx	# A, tmp138
 	addl	%edi, %ecx	# _36, _37
-	addl	%esi, %ebx	# _65, _38
+	addl	%esi, %ebx	# _67, _38
 	movl	%ebx, (%ecx)	# _38, (*_37)[0]
-# main.c:5:     for (j = 0; j < n; j++)
-	addl	$1, -32(%ebp)	#, j
+# main.c:4:     for (j = 0; j < n; j++)
+	addl	$1, -28(%ebp)	#, j
 .L2:
-# main.c:5:     for (j = 0; j < n; j++)
-	movl	-32(%ebp), %ecx	# j, tmp139
+# main.c:4:     for (j = 0; j < n; j++)
+	movl	-28(%ebp), %ecx	# j, tmp139
 	cmpl	8(%ebp), %ecx	# n, tmp139
 	jl	.L3	#,
-# main.c:9:     return A[i][0];
-	movl	%eax, %edx	# n.0_47, n.16_39
-	movl	20(%ebp), %eax	# i, i.17_40
-	imull	%edx, %eax	# n.16_39, _41
+# main.c:7:     return j < n ? A[i][0] : 0;
+	movl	-28(%ebp), %edx	# j, tmp140
+	cmpl	8(%ebp), %edx	# n, tmp140
+	jge	.L4	#,
+# main.c:7:     return j < n ? A[i][0] : 0;
+	movl	%eax, %edx	# n.0_48, n.17_39
+	movl	20(%ebp), %eax	# i, i.18_40
+	imull	%edx, %eax	# n.17_39, _41
 	leal	0(,%eax,4), %edx	#, _42
-	movl	12(%ebp), %eax	# A, tmp140
+	movl	12(%ebp), %eax	# A, tmp141
 	addl	%edx, %eax	# _42, _43
-# main.c:9:     return A[i][0];
-	movl	(%eax), %eax	# (*_43)[0], _62
-# main.c:10: }
+# main.c:7:     return j < n ? A[i][0] : 0;
+	movl	(%eax), %eax	# (*_43)[0], iftmp.16_45
+	jmp	.L6	#
+.L4:
+# main.c:7:     return j < n ? A[i][0] : 0;
+	movl	$0, %eax	#, iftmp.16_45
+.L6:
+# main.c:8: }
 	addl	$20, %esp	#,
 	popl	%ebx	#
 	.cfi_restore 3
@@ -128,9 +133,9 @@ main:
 	.cfi_def_cfa_register 5
 	call	__x86.get_pc_thunk.ax	#
 	addl	$_GLOBAL_OFFSET_TABLE_, %eax	# tmp82,
-# main.c:13:     return 0;
+# main.c:11:     return 0;
 	movl	$0, %eax	#, _1
-# main.c:14: }
+# main.c:12: }
 	popl	%ebp	#
 	.cfi_restore 5
 	.cfi_def_cfa 4, 4
