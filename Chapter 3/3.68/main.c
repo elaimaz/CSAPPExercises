dd31cfec294ad1b66e79d6152e0da62623680a3f
wesley_cazelli <wesley_cazelli@hst.com.br>
2023-11-29 15:44:13 -0300

    :white_check_mark: 3.68 done

--- Chapter 3/3.68/main.c
+++ Chapter 3/3.68/main.c
@@ -1,3 +1,17 @@
+#include <stdio.h>
+
+void good_echo() {
+	char buffer[20];
+	char *line;
+
+	while (fgets(buffer, sizeof(buffer), stdin) != NULL)
+	{
+		printf("%s", buffer);
+	}
+
+}
+
 int main() {
+	good_echo();
     return 0;
 }
