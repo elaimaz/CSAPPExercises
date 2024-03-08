a97a80618e396ec829e1513d5fa59207fe44c7e0
elaimaz <fariawesley4@gmail.com>
2024-03-07 23:13:32 -0300

    :heavy_check_mark: :test_tube: 4.43 enhancing answer and adding tests

--- Chapter 4/4.43/main.c
+++ Chapter 4/4.43/main.c
@@ -1,6 +1,5 @@
 // Compiled with: gcc -m32 main.c -o main
 
-#include <stdio.h>
 #include <assert.h>
 
 void pushl_registers()
@@ -16,25 +15,50 @@ void pushl_registers()
         :"r" (REG)
     );
 
-    printf("esp_before: %d\nesp_after: %d\n", *(esp_before), *(esp_after));
-    printf("esp_before: %p\nesp_after: %p\n", esp_before, esp_after);
-    printf("Memory difference: %d\n", (esp_before - esp_after) * sizeof(int*));
-
     assert((esp_before - esp_after) * sizeof(int*) == 4);
     assert(*(esp_after) == REG);
 }
 
 void pushl_esp()
 {
+    int REG, *esp_before, *esp_after;
+
+    // Testing REG as %esp
     asm(
-        "subl $4, %esp\n\t"
-        "movl %esp, (%esp)"
+        "movl %%esp, %0\n\t"
+        "movl %%esp, %1\n\t"
+        "cmpl %%esp, %0\n\t"
+        "je 1f\n\t"
+        "subl $4, %%esp\n"
+        "1: movl %0, (%%esp)\n\t"
+        "movl %%esp, %2"
+        :"=r" (REG), "=m" (esp_before), "=m" (esp_after)
     );
+
+    assert(esp_after - esp_before == 0);
+
+    // Testing REG as another register
+    asm(
+        "movl $10, %%eax\n\t"
+        "movl %%eax, %0\n\t"
+        "movl %%esp, %1\n\t"
+        "cmpl %%esp, %0\n\t"
+        "je 1f\n\t"
+        "subl $4, %%esp\n"
+        "1: movl %0, (%%esp)\n\t"
+        "movl %%esp, %2"
+        :"=r" (REG), "=m" (esp_before), "=m" (esp_after)
+    );
+
+    assert((esp_before - esp_after) * sizeof(int*) == 4);
+    assert(10 == REG);
+    assert(*(esp_after) == REG);
+
 }
 
 int main()
 {
-    // pushl_registers();
+    pushl_registers();
 
     pushl_esp();
 
